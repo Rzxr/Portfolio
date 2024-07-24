56d4098b69e35b95fda7cd8b9d6516bd712503c2
Rzxbr <r@uktechmail.com>
2024-07-24 16:32:38 +0100

    Setup pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
+import "./styles/App.css";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <div className="container">
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </Router>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import "../styles/Navbar.css";
 
 const Navbar: React.FC = () => {
   return (
-    <nav>
-      <ul>
-        <li><NavLink to="/">Home</NavLink></li>
+    <nav className="navbar">
+
+      <div className="home-container">
+        <NavLink to="/">Home</NavLink>
+      </div>
+
+      <ul className="nav-items">
         <li><NavLink to="/about">About</NavLink></li>
         <li><NavLink to="/projects">Projects</NavLink></li>
         <li><NavLink to="/contact">Contact</NavLink></li>
